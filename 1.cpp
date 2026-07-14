bcecc459ea229d01ddd0c1120c3a61fe1a40f6c1
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-07-14 11:03:45 +0530

    ds-lab1

--- 1.cpp
+++ 1.cpp
@@ -1,13 +1,13 @@
 // Template program 1
 #include<iostream>
 using namespace std;
-template <typename T>
-T large(T n1, T n2){
-    return (n1>n2)?n1:n2;
+template <typename T>                         //template created with a datatype T
+T large(T n1, T n2){                          //parameterised
+    return (n1>n2)?n1:n2;                     //ternary operator
 }
 int main(){
-    cout<<large<int>(3,4)<<endl;
-    cout<<large<float>(4.5,3.2)<<endl;
-    cout<<large<string>("Hai","Ciao")<<endl;
-    return 0;
+    cout<<large<int>(3,4)<<endl;              //object created with datatype int and large func. called
+    cout<<large<float>(4.5,3.2)<<endl;        // "" float ""
+    cout<<large<string>("Hai","Ciao")<<endl;  //object created with datatype string and ascii values of first letter compared, H is bigger so Hai returns
+    return 0;                                    
 }
---
+++ 2.cpp
@@ -0,0 +1,5 @@
+// Template 2
+#include<iostream>
+using namespace std;
+template <typename T>
+class calculator{}
