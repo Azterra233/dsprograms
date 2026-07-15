97a0f072defd9ce8ad0271cb46b71ca6808ece14
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-07-15 09:25:50 +0530

    ds-lab2

---
+++ 4.cpp
@@ -0,0 +1,37 @@
+// Bubble sort
+#include<iostream>
+using namespace std;
+int main(){
+    int a[10],i,n,j,temp,sum=0;
+    cout<<"Enter the no. of array elements: ";
+    cin>>n;
+    cout<<"Enter the array elements:\n";
+    for(i=0;i<n;i++){
+        cout<<"Element "<<i+1<<": ";
+        cin>>a[i];
+        sum+=a[i];
+    }
+    cout<<"Unsorted Array:\n";
+    cout<<"[";
+    for(i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<"]"<<"\n";
+    //bubble sort
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+    cout<<"Sorted Array:\n";
+    cout<<"[";
+    for(i=0;i<n;i++){
+            cout<<a[i]<<" ";
+    }
+    cout<<"]"<<"\n";
+    return 0;
+}
