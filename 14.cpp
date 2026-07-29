4f7485201e11305e80d50e62e9116354f8f7a3cb
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-07-29 10:52:14 +0530

    ds-lab6

--- 14.cpp
+++ 14.cpp
@@ -1,1 +1,5 @@
-// Quick sort
+// Merge sort
+// Ref: https://www.w3schools.com/dsa/dsa_algo_mergesort.php
+// divide n conquer dekho mit divide n conquer fft
+#include<iostream>
+using namespace std;
