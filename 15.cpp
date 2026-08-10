8de336976198c6f68833e6064fba1d198bc3c842
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-08-10 22:30:17 +0530

    ds-lab7
    corrected code
    
    Co-authored-by:  <Azterra233@://github.com>

--- 15.cpp
+++ 15.cpp
@@ -27,7 +27,7 @@ void quicksort(int a[], int lb, int ub){
 }
 // main function - execution starts here
 int main(){
-    int a[20],n,;
+    int a[20],n,i;
     cout<<"Enter the no. of elements: ";
     cin>>n;
     cout<<"Enter elements:\n";
