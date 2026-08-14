ba0411ccc80241639b05f1b9570788f6637dfb42
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-08-14 13:03:16 +0530

    ds-lab9

--- 17.cpp
+++ 17.cpp
@@ -1,4 +1,16 @@
 // Selection Sort
 #include<iostream>
 using namespace std;
+int selectionsort(int a[], int lb, int ub){
+    int i,n,minindex;
+    n = ub - lb + 1;
+    for(i=0;i<n-1;i++){
+        minindex = i;
+        for(){
+            
+        }
 
+    }
+
+
+}
