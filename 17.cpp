1ec03da2efd52bacd9d606ed7b439c6608a724ed
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-08-17 09:53:27 +0530

    ds-lab10

--- 17.cpp
+++ 17.cpp
@@ -1,16 +1,60 @@
 // Selection Sort
-#include<iostream>
+#include <iostream>
 using namespace std;
-int selectionsort(int a[], int lb, int ub){
-    int i,n,minindex;
+
+void selectionsort(int a[], int lb, int ub)
+{
+    int i, j, n, minindex;
+
     n = ub - lb + 1;
-    for(i=0;i<n-1;i++){
+
+    for (i = 0; i < n - 1; i++)
+    {
         minindex = i;
-        for(){
-            
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[lb + j] < a[lb + minindex])
+            {
+                minindex = j;
+            }
         }
 
+        swap(a[lb + minindex], a[lb + i]);
+    }
+}
+
+int main()
+{
+    int a[20], n, i;
+
+    cout << "Enter the no. of array elements: ";
+    cin >> n;
+
+    cout << "Enter elements:\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "Element " << i + 1 << ": ";
+        cin >> a[i];
     }
 
+    cout << "Unsorted Array:\n";
+    cout << "[";
+    for (i = 0; i < n; i++)
+    {
+        cout << " " << a[i];
+    }
+    cout << " ]\n";
 
+    selectionsort(a, 0, n - 1);
+
+    cout << "Sorted Array:\n";
+    cout << "[";
+    for (i = 0; i < n; i++)
+    {
+        cout << " " << a[i];
+    }
+    cout << " ]\n";
+
+    return 0;
 }
