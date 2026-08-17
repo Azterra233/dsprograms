903c39f36471ba22d757eb82ff65f9ec27de4042
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-08-17 19:20:43 +0530

    ds-lab10

--- 17.cpp
+++ 17.cpp
@@ -6,13 +6,13 @@ void selectionsort(int a[], int lb, int ub)
 {
     int i, j, n, minindex;
 
-    n = ub - lb + 1;
+    n = ub - lb + 1;                        //range: calculates the total count of elements within the given boundary [lb, ub]
 
-    for (i = 0; i < n - 1; i++)
+    for (i = 0; i < n - 1; i++)             //Outer loop running from the first element up to the second-to-last element (n - 2).
     {
-        minindex = i;
+        minindex = i;                       //minindex stores min. index; temporarily assumes the first element of the unsorted segment is the minimum.
 
-        for (j = i + 1; j < n; j++)
+        for (j = i + 1; j < n; j++)         //Inner loop scanning the remaining unsorted elements starting at i + 1.
         {
             if (a[lb + j] < a[lb + minindex])
             {
@@ -38,7 +38,7 @@ int main()
         cin >> a[i];
     }
 
-    cout << "Unsorted Array:\n";
+    cout << "\nUnsorted Array:\n";
     cout << "[";
     for (i = 0; i < n; i++)
     {
@@ -46,9 +46,9 @@ int main()
     }
     cout << " ]\n";
 
-    selectionsort(a, 0, n - 1);
+    selectionsort(a, 0, n - 1);                       //Calls the sorting function on array a with lower bound 0 and upper bound n - 1.
 
-    cout << "Sorted Array:\n";
+    cout << "\nSorted Array:\n";
     cout << "[";
     for (i = 0; i < n; i++)
     {
