d3b7c1651fe0503bbfa0219d460e95d0b7e1c44f
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-09-08 12:59:15 +0530

    ds-lab11

--- 18.cpp
+++ 18.cpp
@@ -1,50 +1,67 @@
 // Write a cppp to create and display a linked list
 #include<iostream>
 using namespace std;
-class Linkedlist{
-    struct node{
+
+class Linkedlist {
+    struct node {
         int data;
         node *link;
-    } *start,*newnode,*temp;                //structure variables
+    } *start, *newnode, *temp;                // structure variables
+    
     public:
-        Linkedlist();                       //default constructor
-        void insert();                      //function prototype
+        Linkedlist();                       // default constructor
+        void insert();                      // function prototype
         void display();
 };
-void Linkedlist(){
-    start  = NULL;
+
+// Fixed constructor definition
+Linkedlist::Linkedlist() {
+    start = NULL;
+    newnode = NULL;
+    temp = NULL;
 }
-void Linkedlist()::insert(){
+
+// Fixed scope resolution syntax for insert
+void Linkedlist::insert() {
     int num;
     char ch = 'y';
-    do{
-        cout<<"Enter the number: ";
-        cin>>num;
+    do {
+        cout << "Enter the number: ";
+        cin >> num;
         newnode = new node;
-        newnode->data=num;  
-        newnode->link=NULL;
-        if(start=NULL){
+        newnode->data = num;  
+        newnode->link = NULL;
+        
+        // Fixed assignment (=) to comparison (==)
+        if (start == NULL) {
             start = temp = newnode;
-        }else{
-            temp->link=newnode;
+        } else {
+            temp->link = newnode;
             temp = newnode;
         }
-        cout<<"Do you wanna continue: ";
-        cin>>ch;
-    }while(ch=='y');
+        
+        cout << "Do you wanna continue (y/n): ";
+        cin >> ch;
+    } while (ch == 'y');
 }
-void Linkedlist()::display(){
+
+// Fixed scope resolution syntax for display
+void Linkedlist::display() {
     temp = start;
-    if(start==NULL){
-        cout<<"Linked list is empty\n";
-    }else{
-        while(temp!=NULL){
-            cout<<temp->data<<"\n";
+    if (start == NULL) {
+        cout << "Linked list is empty\n";
+    } else {
+        while (temp != NULL) {
+            cout << temp->data << " -> ";
             temp = temp->link;
         }
-        cout<<"END";
+        cout << "\nEND\n";
     }
 }
 
-
-
+int main() {
+    Linkedlist list;
+    list.insert();
+    list.display();
+    return 0;
+}
